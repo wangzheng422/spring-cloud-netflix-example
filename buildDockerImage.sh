afdaaa4f6a0afdd22982991da795bc3dbeb24ffa
wzh <wangzheng422@foxmail.com>
2018-03-17 10:22:29 +0800

    alauda

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -5,5 +5,6 @@ set -eo pipefail
 modules=( admin-dashboard config-server eureka-server hystrix-dashboard service-a service-b zuul )
 
 for module in "${modules[@]}"; do
-    docker build -t "spring-cloud-netflix-example/${module}:latest" ${module}
+    docker build -t "10.11.0.8:5000/spring-cloud-netflix-example-${module}:latest" ${module}
+    docker push "10.11.0.8:5000/spring-cloud-netflix-example-${module}:latest"
 done
