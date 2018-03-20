7b395377fceb776d0ad308ad90f1ce2f96d655b4
wzh <wangzheng422@foxmail.com>
2018-03-20 13:23:30 +0800

    hystrix

--- service-a/src/main/java/net/devh/hystrix/HystrixWrappedServiceBClient.java
+++ service-a/src/main/java/net/devh/hystrix/HystrixWrappedServiceBClient.java
@@ -1,11 +1,12 @@
 package net.devh.hystrix;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-
-import net.devh.feign.ServiceBClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import net.devh.feign.ServiceBClient;
 
 /**
  * User: Michael
@@ -19,7 +20,24 @@ public class HystrixWrappedServiceBClient implements ServiceBClient {
     private ServiceBClient serviceBClient;
 
     @Override
-    @HystrixCommand(groupKey = "helloGroup", fallbackMethod = "fallBackCall")
+    @HystrixCommand(groupKey = "helloGroup", fallbackMethod = "fallBackCall",
+            commandProperties = {
+                @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500"),
+                @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                @HystrixProperty(name = "execution.isolation.semaphore.maxConcurrentRequests", value = "1"),
+                //@HystrixProperty(name = "execution.isolation.strategy", value = "SEMAPHORE"),
+                @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "5000")
+            },
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "1"),
+                    @HystrixProperty(name = "maxQueueSize", value = "1"),
+                    @HystrixProperty(name = "allowMaximumSizeToDivergeFromCoreSize", value = "true"),
+                    @HystrixProperty(name = "keepAliveTimeMinutes", value = "2"),
+                    @HystrixProperty(name = "queueSizeRejectionThreshold", value = "1"),
+                    @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "10"),
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "5000")
+    })
     public String printServiceB() {
         return serviceBClient.printServiceB();
     }
