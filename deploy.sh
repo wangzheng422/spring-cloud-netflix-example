b3245c582844e38df6abc027136b052c8fbc390f
wzh <wangzheng422@gmail.com>
2018-04-18 14:13:10 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,13 @@ for module in "${modules[@]}"; do
     docker build -t "${REGISTRY}/spring-cloud-netflix-example-${module}:latest" ${module}
     docker push "${REGISTRY}/spring-cloud-netflix-example-${module}:latest"
 done
+
+sed docker-compose.alauda.yml | \
+sed "s/{{ALB_IP}}/${ALB_IP}/g" | \
+sed "s/{{GIT_HOST}}/${GIT_HOST}/g" | \
+sed "s/{{REGISTRY}}/${REGISTRY}/g" | \
+sed "s/{{REGION}}/${REGION}/g" | \
+sed "s/{{NGINX}}/${NGINX}/g"  \
+> tmp/alauda-tmp.yaml
+
+echo "use alauda-tmp.yaml to deploy the app"
